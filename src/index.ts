3df7144c2f4fa0d61aadd7a344ce1e47adb4f0f3
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2023-02-24 20:31:25 +1100

    mysql

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import express from "express";
+import mysql from "mysql";
+import * as dotenv from "dotenv";
 import { json } from "body-parser";
 import { setsRouter } from "./routes/sets";
+
+dotenv.config();
 
 const app = express();
 
 app.use(json());
 app.use(setsRouter);
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PW,
+  database: "pogobuff",
+});
+
+connection.connect();
+
+// connection.end();
 
 app.listen(3001, () => {
   console.log("Pogobuff running on port 3001");
